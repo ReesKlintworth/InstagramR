b357325a2ff8b34b25add13ecbfb67f75ac84264
Rees Klintworth <reesk5150@gmail.com>
2014-11-04 11:55:16 -0600

    Nested tabsetPanels to group by user and hashtag

--- App/ui.R
+++ App/ui.R
@@ -1,20 +1,36 @@
 library(rCharts)
 library(shiny)
 
+textInputRow<-function (inputId, label, value = "") 
+{
+  div(style="display:inline-block",
+      tags$label(label, `for` = inputId), 
+      tags$input(id = inputId, type = "text", value = value, class="input-medium"))
+}
+
+numericInputRow <- function (inputId, label, value, min)
+{
+  div(style="display:inline-block",
+      tags$label(label, `for` = inputId),
+      tags$input(id = inputId, type="number", value=value, min = min, step=1, class="input-small"))
+}
+
 shinyUI(fluidPage(
   titlePanel("InstagramR"),
   
-  sidebarPanel(
-    textInput("username", "Instagram Username:", "rkade93"),
-    numericInput("picture_number", "How many pictures?", 20, min=1),
-    submitButton("Submit")
-  ),
-  
   mainPanel(
     tabsetPanel(
-      tabPanel("User Plot", showOutput("plot", "highcharts")),
-      tabPanel("User Map", showOutput("map", "leaflet")),
-      tabPanel("Hashtag Map", showOutput("map2", "leaflet"))
-    )
+      tabPanel("User",
+        textInputRow("username", "Instagram Username:", "rkade93"),
+        numericInputRow("picture_number", "How many pictures?", 20, min=1),
+        submitButton("Submit"),
+        tags$div(style="margin-bottom:15px;"),
+        tabsetPanel(
+          tabPanel("Plot", showOutput("plot", "highcharts")),
+          tabPanel("User Map", showOutput("map", "leaflet"))
+        )
+      ),
+      tabPanel("Hashtag", showOutput("map2", "leaflet"))
+    ), width=12
   )
 ))
