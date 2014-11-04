2a379634484d9c871c1b1a69a0a4ed28da440fd5
Rees Klintworth <reesk5150@gmail.com>
2014-11-04 14:03:37 -0600

    Code cleanup, reduction of duplicate logic

--- App/server.R
+++ App/server.R
@@ -3,44 +3,50 @@ library(rjson)
 library(RCurl)
 library(shiny)
 
+app_name <- "Media and User Analysis"
+client_id <- "432555bd340f486281d3a28bd4bd3a9a"
+client_secret <- "33220c4d9e9d4e8ab513b21fd842f06e"
+scope <- "basic"
+token <- "13705907.432555b.4ab9b08b546546dfa7cdd0342dcc5842"
+
+recent_pictures_for_user <- function(input){
+  username <- input$username
+  user_url <- paste0("https://api.instagram.com/v1/users/search?q=", username, "&access_token=", token)
+  user_info <- fromJSON(getURL(user_url), unexpected.escape="keep")
+  
+  user_profile <- user_info$data[[1]]
+  
+  user_id <- user_profile$id
+  
+  picture_number <- input$picture_number
+  
+  urls <- ceiling(picture_number / 20)
+  
+  next_url <- paste0("https://api.instagram.com/v1/users/", user_id, "/media/recent/?client_id=",client_id)
+  
+  recent_pictures <- list()
+  counter = 0
+  for(counter in 1:urls)
+  {
+    if (!is.null(next_url))
+    {
+      if (counter == urls && picture_number%%20 != 0)
+      {
+        next_url <- paste0(next_url, "&count=", picture_number%%20)
+      }
+      recent_posts <- fromJSON(getURL(next_url), unexpected.escape="keep")
+      recent_pictures <- append(recent_pictures, recent_posts$data)
+      next_url <- recent_posts$pagination$next_url
+    }
+  }
+  recent_pictures <- rev(recent_pictures)
+}
+
 shinyServer(function(input, output){
-  app_name <- "Media and User Analysis"
-  client_id <- "432555bd340f486281d3a28bd4bd3a9a"
-  client_secret <- "33220c4d9e9d4e8ab513b21fd842f06e"
-  scope <- "basic"
-  token <- "13705907.432555b.4ab9b08b546546dfa7cdd0342dcc5842"
   
   output$plot <- renderChart({
-    username <- input$username
-    user_url <- paste0("https://api.instagram.com/v1/users/search?q=", username, "&access_token=", token)
-    user_info <- fromJSON(getURL(user_url), unexpected.escape="keep")
-  
-    user_profile <- user_info$data[[1]]
-  
-    user_id <- user_profile$id
     
-    picture_number <- input$picture_number
-    
-    urls <- ceiling(picture_number / 20)
-    
-    next_url <- paste0("https://api.instagram.com/v1/users/", user_id, "/media/recent/?client_id=",client_id)
-    
-    recent_pictures <- list()
-    counter = 0
-    for(counter in 1:urls)
-    {
-      if (!is.null(next_url))
-      {
-        if (counter == urls && picture_number%%20 != 0)
-        {
-          next_url <- paste0(next_url, "&count=", picture_number%%20)
-        }
-        recent_posts <- fromJSON(getURL(next_url), unexpected.escape="keep")
-        recent_pictures <- append(recent_pictures, recent_posts$data)
-        next_url <- recent_posts$pagination$next_url
-      }
-    }
-    recent_pictures <- rev(recent_pictures)
+    recent_pictures <- recent_pictures_for_user(input)
     picture_number <- length(recent_pictures)
     
     likes_comments_df = data.frame( number=1:length(recent_pictures))
@@ -66,36 +72,7 @@ shinyServer(function(input, output){
   })
   
   output$map <- renderMap({
-    username <- input$username
-    user_url <- paste0("https://api.instagram.com/v1/users/search?q=", username, "&access_token=", token)
-    user_info <- fromJSON(getURL(user_url), unexpected.escape="keep")
-    
-    user_profile <- user_info$data[[1]]
-    
-    user_id <- user_profile$id
-    
-    picture_number <- input$picture_number
-    
-    urls <- ceiling(picture_number / 20)
-    
-    next_url <- paste0("https://api.instagram.com/v1/users/", user_id, "/media/recent/?client_id=",client_id)
-    
-    recent_pictures <- list()
-    counter = 0
-    for(counter in 1:urls)
-    {
-      if (!is.null(next_url))
-      {
-        if (counter == urls && picture_number%%20 != 0)
-        {
-          next_url <- paste0(next_url, "&count=", picture_number%%20)
-        }
-        recent_posts <- fromJSON(getURL(next_url), unexpected.escape="keep")
-        recent_pictures <- append(recent_pictures, recent_posts$data)
-        next_url <- recent_posts$pagination$next_url
-      }
-    }
-    recent_pictures <- rev(recent_pictures)
+    recent_pictures <- recent_pictures_for_user(input)
     picture_number <- length(recent_pictures)
     
     map <- Leaflet$new()
--- App/ui.R
+++ App/ui.R
@@ -1,15 +1,13 @@
 library(rCharts)
 library(shiny)
 
-textInputRow<-function (inputId, label, value = "") 
-{
+textInputRow<-function (inputId, label, value = ""){
   div(style="display:inline-block",
       tags$label(label, `for` = inputId), 
       tags$input(id = inputId, type = "text", value = value, class="input-medium"))
 }
 
-numericInputRow <- function (inputId, label, value, min)
-{
+numericInputRow <- function (inputId, label, value, min){
   div(style="display:inline-block",
       tags$label(label, `for` = inputId),
       tags$input(id = inputId, type="number", value=value, min = min, step=1, class="input-small"))
