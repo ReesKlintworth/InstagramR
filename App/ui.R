221f506ddb3777de26ac1eeb21998c34a38570a0
Rees Klintworth <reesk5150@gmail.com>
2014-11-11 11:26:59 -0600

    Switched to using navbarPage layout

--- App/ui.R
+++ App/ui.R
@@ -1,21 +1,17 @@
-shinyUI(fluidPage(
-  titlePanel("InstagramR"),
-  
-  tabsetPanel(
-    tabPanel("User Analysis",
-      textInputRow("username", "Instagram Username:", "rkade93"),
-      numericInputRow("picture_number", "How many pictures?", 20, min=1),
-      actionButtonSeparateRow("update_user_data", "Update Data"),
-      tags$div(style="margin-bottom:15px;"),
-      tabsetPanel(
-        tabPanel("Plot", showOutput("user_chart", "highcharts")),
-        tabPanel("User Map", showOutput("user_map", "leaflet"))
-      )
-    ),
-    tabPanel("Hashtag Analysis",
-      textInputRow("hashtag", "Hashtag:", "RkadeKicks"),
-      actionButtonSeparateRow("update_hashtag_data", "Update Data"),
-      tags$div(style="margin-bottom:15px;"),
-      showOutput("map2", "leaflet"))
-  )
+shinyUI(navbarPage("InstagramR",
+  tabPanel("User Analysis",
+    textInputRow("username", "Instagram Username:", "rkade93"),
+    numericInputRow("picture_number", "How many pictures?", 20, min=1),
+    actionButtonSeparateRow("update_user_data", "Update Data"),
+    tags$div(style="margin-bottom:15px;"),
+    tabsetPanel(
+      tabPanel("Plot", showOutput("user_chart", "highcharts")),
+      tabPanel("User Map", showOutput("user_map", "leaflet"))
+    )
+  ),
+  tabPanel("Hashtag Analysis",
+    textInputRow("hashtag", "Hashtag:", "RkadeKicks"),
+    actionButtonSeparateRow("update_hashtag_data", "Update Data"),
+    tags$div(style="margin-bottom:15px;"),
+    showOutput("map2", "leaflet"))
 ))
